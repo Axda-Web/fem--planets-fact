d1ae82e1a51677a76eb61385cfc00fbc28924ee4
Alyx DARENNE <axda.web@gmail.com>
2023-11-06 14:20:33 +0100

    Planet page: Add planet page components

---
+++ app/components/PlanetTab.tsx
@@ -0,0 +1,79 @@
+import clsx from "clsx";
+
+type PlanetTabProps = {
+  index: string;
+  planet: string;
+  label: string;
+  value: string;
+  isActive: boolean;
+  handleTabClick: (tab: string) => void;
+};
+
+const PlanetTab = ({
+  index,
+  planet,
+  label,
+  value,
+  isActive,
+  handleTabClick,
+}: PlanetTabProps) => {
+  const mobileLabel =
+    value === "structure"
+      ? "Sructure"
+      : value === "geology"
+      ? "Surface"
+      : label;
+
+  const mobileComputedClasses: Record<string, string> = {
+    mercury: "after:bg-mercury",
+    venus: "after:bg-venus",
+    earth: "after:bg-earth",
+    mars: "after:bg-mars",
+    jupiter: "after:bg-jupiter",
+    saturn: "after:bg-saturn",
+    uranus: "after:bg-uranus",
+    neptune: "after:bg-neptune",
+  };
+
+  const computedClasses: Record<string, string> = {
+    mercury: "md:bg-mercury md:border-mercury",
+    venus: "md:bg-venus md:border-venus",
+    earth: "md:bg-earth md:border-earth",
+    mars: "md:bg-mars md:border-mars",
+    jupiter: "md:bg-jupiter md:border-jupiter",
+    saturn: "md:bg-saturn md:border-saturn",
+    uranus: "md:bg-uranus md:border-uranus",
+    neptune: "md:bg-neptune md:border-neptune",
+  };
+
+  return (
+    <button
+      onClick={() => handleTabClick(value)}
+      className={clsx(
+        "font-paragraph font-bold text-light uppercase text-[9px] tracking-[1.929px] transition-colors",
+        "md:py-3 md:px-7 md:border md:hover:border-carbonBlue md:hover:bg-carbonBlue md:text-xs md:w-full md:flex md:gap-x-6",
+        {
+          "md:border-light": !isActive,
+          [computedClasses[planet]]: isActive,
+        }
+      )}
+    >
+      <span className={clsx("text-light/50 hidden", "md:block")}>{index}</span>{" "}
+      <span
+        className={clsx(
+          "block py-5 px-2 relative",
+          "after:h-1 after:absolute after:-bottom-0 after:w-full after:left-0 after:transition-colors",
+          {
+            "after:bg-transparent text-light/50": !isActive,
+            [mobileComputedClasses[planet]]: isActive,
+          },
+          "md:hidden"
+        )}
+      >
+        {mobileLabel}
+      </span>
+      <span className={clsx("hidden", "md:block")}>{label}</span>
+    </button>
+  );
+};
+export default PlanetTab;
--- app/planets/[planetName]/page.tsx
+++ app/planets/[planetName]/page.tsx
@@ -1,4 +1,70 @@
-const PlanetPage = () => {
-  return <div>PlanetPage</div>;
+import clsx from "clsx";
+
+import { getPlanets, getPlanetByName } from "@/lib/actions/planet";
+import PlanetInfoSection from "@/app/components/PlanetInfoSection";
+import NumberCard from "@/app/components/NumberCard";
+
+type Props = {
+  params: { planetName: string };
 };
+export async function generateMetadata({ params }: Props) {
+  const planet = await getPlanetByName(params.planetName);
+  return {
+    title: `Planets | ${planet?.name}`,
+    description: `Discover the secrets of ${planet?.name}!`,
+  };
+}
+
+export async function generateStaticParams() {
+  const planets = await getPlanets();
+  return planets?.map((planet) => ({
+    planetName: planet.name,
+  }));
+}
+
+async function PlanetPage({ params }: Props) {
+  const planet = await getPlanetByName(params.planetName);
+  const trimmedData = Object.entries(planet!)
+    .filter(
+      ([key, value]) =>
+        key === "rotation" ||
+        key === "revolution" ||
+        key === "radius" ||
+        key === "temperature"
+    )
+    .map(([key, value]) => {
+      if (key === "rotation" || key === "revolution") {
+        return [key + " time", value];
+      } else if (key === "temperature") {
+        return ["Average Temp.", value];
+      } else {
+        return [key, value];
+      }
+    });
+
+  return (
+    <main
+      className={clsx(
+        "md:px-10 md:max-w-[768px] mx-auto",
+        "xl:max-w-[1110px] xl:pt-32"
+      )}
+    >
+      <PlanetInfoSection planet={planet!} />
+      <section
+        className={clsx(
+          "px-6 flex flex-col gap-y-2 mt-6",
+          "md:px-0 md:flex-row gap-x-[11px]"
+        )}
+      >
+        {trimmedData.map(([title, value], index) => (
+          <NumberCard
+            key={index}
+            title={title as string}
+            value={value as string}
+          />
+        ))}
+      </section>
+    </main>
+  );
+}
 export default PlanetPage;
